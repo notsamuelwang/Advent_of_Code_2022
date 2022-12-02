f6f32733daa7f799ad1fa4d918b6e9af3f3dc6ec
Samuel Wang <sam@Samuels-MacBook-Pro.local>
2022-12-02 00:29:24 -0500

    day2

--- day1b.py
+++
@@ -1,9 +0,0 @@
-f = open("day1input.txt")
-a=[0]
-for l in f.readlines():
-    l = l.removesuffix('\n')
-    if l=='':
-        a.append(0)
-    else:
-        a[-1]+=int(l)
-print(sum(sorted(a)[-3:]))
