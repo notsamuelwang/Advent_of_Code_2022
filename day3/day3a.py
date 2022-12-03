cd2293313c5be3e55a60b18cb4200474969ebf7a
notsamuelwang <notsamuelwang@gmail.com>
2022-12-03 00:02:44 -0500

    day3

--- day3/day3a.py
+++ day3/day3a.py
@@ -1,1 +1,13 @@
-a
+f=open("day3input.txt")
+ans = 0
+for l in f.readlines():
+    ans = 0
+    x = l[:len(l)/2]
+    y = l[:len(l)/2]
+    for i in x:
+        if i in y:
+            if i==i.lower():
+                ans+=ord(i)-ord('a')+1
+            else:
+                ans+=ord(i)-ord('A')+27
+print(ans)
