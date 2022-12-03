839a869048cee598e33c18751e0ed838e34232ae
notsamuelwang <notsamuelwang@gmail.com>
2022-12-03 00:49:25 -0500

    day3

--- day3/day3b.py
+++ day3/day3b.py
@@ -1,12 +1,16 @@
 f=open("./day3/day3input.txt")
-ans = 0
+a=[]
 for l in f.readlines():
-    l = l.removesuffix("\n")
-    x = l[:len(l)//2]
-    y = l[len(l)//2:]
+    a.append(l.removesuffix("\n"))
+
+ans = 0
+for i in range(len(a)//3):
+    x = a[3*i]
+    y = a[3*i+1]
+    z = a[3*i+2]
     found = False
     for i in x:
-        if i in y and not found:
+        if i in y and i in z and not found:
             if i==i.lower():
                 ans+=ord(i)-ord('a')+1
             else:
